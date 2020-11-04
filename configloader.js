d8db15d397128f380ed9734692a169a765c50236
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2020-11-05 02:33:06 +0900

    check plugins/rules

--- configloader.js
+++ configloader.js
@@ -4,20 +4,24 @@ result = rcFile.rcFile("textlint", {
     cwd: process.env.GITHUB_WORKSPACE
 });
 
-for (const [key, value] of Object.entries(result.config.plugins)) {
-    if (value != false) {
-        if (key.startsWith("@")) {
-            const scope = key.slice(0, key.indexOf("/"));
-            const name = key.slice(key.indexOf('/') + 1);
-            console.log(`${scope}/textlint-plugin-${name}`);
-        } else {
-            console.log(`textlint-plugin-${key}`);
+if (result.config.plugins) {
+    for (const [key, value] of Object.entries(result.config.plugins)) {
+        if (value != false) {
+            if (key.startsWith("@")) {
+                const scope = key.slice(0, key.indexOf("/"));
+                const name = key.slice(key.indexOf('/') + 1);
+                console.log(`${scope}/textlint-plugin-${name}`);
+            } else {
+                console.log(`textlint-plugin-${key}`);
+            }
         }
     }
 }
 
-for (const [key, value] of Object.entries(result.config.rules)) {
-    if (value != false) {
-        console.log(`textlint-rule-${key}`)
+if (result.config.rules) {
+    for (const [key, value] of Object.entries(result.config.rules)) {
+        if (value != false) {
+            console.log(`textlint-rule-${key}`)
+        }
     }
 }
