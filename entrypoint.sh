15156b4f019ed7cbc9a6a89d1980227f2dd583c1
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2020-11-06 01:44:14 +0900

    FETCH_HEAD?

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,7 +35,7 @@ PR_NO=$(jq -r '.number' $GITHUB_EVENT_PATH)
 
 git fetch origin "refs/pull/${PR_NO}/head"
 
-(git diff --name-only "pull/${PR_NO}/head" HEAD | xargs "$TEXTLINT_BIN" -f @kounoike/textlint-formatter-rdjsonl "${INPUT_TEXTLINT_FLAGS}") | tee rd.jsonl
+(git diff --name-only FETCH_HEAD HEAD | xargs "$TEXTLINT_BIN" -f @kounoike/textlint-formatter-rdjsonl "${INPUT_TEXTLINT_FLAGS}") | tee rd.jsonl
 cat rd.jsonl \
       | reviewdog -f=rdjsonl                            \
         -name="${INPUT_TOOL_NAME}"                      \
