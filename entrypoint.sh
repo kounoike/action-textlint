165b5a52b8d329853061a62a38d84ff4398e952b
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2020-11-16 21:41:46 +0900

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,7 +31,8 @@ echo -n "textlint version: "
 "$TEXTLINT_BIN" --version || exit 1
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
-"$TEXTLINT_BIN" -f @kounoike/textlint-formatter-rdjsonl "${INPUT_TEXTLINT_FLAGS}" \
+"$TEXTLINT_BIN" -f @kounoike/textlint-formatter-rdjsonl "${INPUT_TEXTLINT_FLAGS}" | tee reviewdog.jsonl
+cat reviewdog.jsonl \
       | reviewdog -f=rdjsonl                            \
         -name="${INPUT_TOOL_NAME}"                      \
         -reporter="${INPUT_REPORTER:-github-pr-review}" \
